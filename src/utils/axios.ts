85cd18dd6489afa2308c23c948f110dc019a91f6
odado <heroteam.pl@gmail.com>
2023-05-03 09:38:20 +0200

    Added getMovieDetails function which getting data from SWAPI

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,3 +1,8 @@
 import axios from 'axios'
 
 export const getAxios = () => axios.create()
+
+export const getMovieDetails = async (moveId: number) => {
+    const { data } = await getAxios().get(`https://swapi.dev/api/films/${moveId}`)
+    return data
+}
