79f8c871a14ad89e6b762984b1b1e8e7d9551681
odado <heroteam.pl@gmail.com>
2023-05-05 23:03:33 +0200

    Added getCharacterDetails function which getting data from SWAPI

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,3 +6,11 @@ export const getMovieDetails = async (movieId: number) => {
     const { data } = await getAxios().get(`https://swapi.dev/api/films/${movieId}`)
     return data
 }
+
+export const getCharacterDetails = async (characterUrl: string) => {
+    const { data } = await getAxios().get(characterUrl)
+    return {
+        characterId: parseInt(data.url.split('/')[5]),
+        name: data.name
+    }
+}
